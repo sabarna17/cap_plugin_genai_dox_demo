6602b740f6abede8474115057432acb5b3b1f8bd
Sabarna Chatterjee <39834671+sabarna17@users.noreply.github.com>
2024-09-12 10:53:26 +0530

    Update lib_cap_dox.js

--- cap_dox/lib_cap_dox.js
+++ cap_dox/lib_cap_dox.js
@@ -19,22 +19,22 @@ async function createPdfFile(pdfBuffer, outputPath) {
   });
 }
 
-async function setbody(pdf,fileName,auth_token){
+async function setbody(pdf, fileName, auth_token) {
   let mydata = new FormData();
   fileName = './' + fileName;
   await createPdfFile(pdf, fileName);
-  mydata.append('file', fs.createReadStream(fileName));  
+  mydata.append('file', fs.createReadStream(fileName));
   cap_dox_job = {
     "schemaId": await get_schema(auth_token),
-    "clientId":cap_dox_config.clientId,
+    "clientId": cap_dox_config.clientId,
     "documentType": cap_dox_config.documentType
   }
-  mydata.append('options', JSON.stringify(cap_dox_job, null, 2) )
+  mydata.append('options', JSON.stringify(cap_dox_job, null, 2))
   // log(mydata)
   return mydata
 }
 
-async function get_token(){
+async function get_token() {
   var basic_auth = cap_dox_key.uaa.clientid + ':' + cap_dox_key.uaa.clientsecret
   let config = {
     method: 'get',
@@ -57,11 +57,11 @@ async function get_token(){
   return 'Bearer ' + access_token;
 }
 
-async function get_schema(auth_token){
+async function get_schema(auth_token) {
   let config = {
     method: 'get',
     maxBodyLength: Infinity,
-    url: cap_dox_key.endpoints.backend.url +  cap_dox_key.swagger + 'schemas?clientId=' + cap_dox_config.clientId,
+    url: cap_dox_key.endpoints.backend.url + cap_dox_key.swagger + 'schemas?clientId=' + cap_dox_config.clientId,
     headers: {
       'Authorization': auth_token,
       'Accept': 'application/json'
@@ -71,32 +71,32 @@ async function get_schema(auth_token){
   let schemaId = '';
   schemaId = await axios.request(config)
     .then((response) => {
-      for (let item of response.data.schemas){
+      for (let item of response.data.schemas) {
         // log('------------',item,'-------------',item.documentType)
-        if((item.name === cap_dox_config.schemaName) && (item.documentType === cap_dox_config.documentType)) {
+        if ((item.name === cap_dox_config.schemaName) && (item.documentType === cap_dox_config.documentType)) {
           return item.id;
-        } 
+        }
       }
     })
     .catch((error) => {
       log(error);
-  });
+    });
   log('Schema ID: ', schemaId)
   return schemaId;
 }
 
-async function post_job(pdf,fileName,auth_token){
+async function post_job(pdf, fileName, auth_token) {
   // PDF file store in local
-  var job_data = await setbody(pdf,fileName,auth_token);
+  var job_data = await setbody(pdf, fileName, auth_token);
   let config = {
     method: 'post',
     maxBodyLength: Infinity,
-    url: cap_dox_key.endpoints.backend.url +  cap_dox_key.swagger + 'document/jobs',
-    headers: { 
+    url: cap_dox_key.endpoints.backend.url + cap_dox_key.swagger + 'document/jobs',
+    headers: {
       'Authorization': auth_token,
       'Accept': 'application/json'
     },
-    data : job_data
+    data: job_data
   };
 
   let job_id = '';
@@ -112,81 +112,114 @@ async function post_job(pdf,fileName,auth_token){
   return job_id;
 }
 
-async function get_job_status(job_id, auth_token){
+async function get_job_status(job_id, auth_token) {
   let config = {
     method: 'get',
     maxBodyLength: Infinity,
-    url: cap_dox_key.endpoints.backend.url +  cap_dox_key.swagger + 'document/jobs/' + job_id,
+    url: cap_dox_key.endpoints.backend.url + cap_dox_key.swagger + 'document/jobs/' + job_id,
     headers: { 'Authorization': auth_token }
   };
 
   var retry_count = 0;
-  for(;;)
-  {
-    
+  for (; ;) {
+
     var job_details = await axios.request(config)
-    .then((response) => {
-      log('JOB Status Data: ------------------>')
-      log(JSON.stringify(response.data));
-      return response.data;
-    })
-    .catch((error) => {
-      log(error);
-    });
-    var job_status = job_details.status;
-    if(job_status){
-      if(job_status === 'DONE'){
-        return job_details
-      }
-      else{
-        // await setTimeout(() =>{ console.log('5 seconds have passed!'); }, 5000);
-        retry_count = retry_count + 1;
-        if(retry_count > 100){
+      .then((response) => {
+        log('JOB Status: --->', JSON.stringify(response.data.status));
+        return response.data;
+      })
+      .catch((error) => {
+        log(error);
+      });
+
+    try {
+      var job_status = job_details.status;
+      if (job_status) {
+        if (job_status === 'DONE') {
           return job_details
+        }
+        else {
+          // await setTimeout(() =>{ console.log('5 seconds have passed!'); }, 5000);
+          retry_count = retry_count + 1;
+          if (retry_count > 100) {
+            return job_details
+          }
         }
       }
     }
-  }
-}
-
-async function entity_mapping_head_def(headerFields, entity){
-  for(let item of headerFields){
-    entity[item.name] = item.rawValue
-  }
-  return entity
-}
-
-async function entity_mapping_item_def(lineItems, entity){
-  let ingredients = [];
-  let ingredient = {};
-  for(let item of lineItems){
-    for(let item_properties of item){
-      ingredient[item_properties.name] = item_properties.rawValue
+    catch (err) {
+      log(JSON.stringify(err))
     }
-    ingredients.push(ingredient)
+
   }
-  entity.ingredients = ingredients
-  // log(entity)
+}
+
+async function entity_mapping_head_def(headerFields, entity) {
+  for (let item of headerFields) {
+    entity[item.name] = item.rawValue
+    log('Value Mapped \n', item.name, '--->', item.rawValue);
+  }
   return entity
 }
 
-module.exports = {  
-  auth_token: async function() { 
+async function entity_mapping_item_def(lineItems, entity) {
+  log('Started mapping items.')
+  log('entity: ', entity)
+  try {
+    let entity_properties = [];
+    for (const key in entity) {
+      const value = entity[key];
+      let valueType;
+
+      if (Array.isArray(value)) {
+        valueType = 'array';
+      } else if (value === null) {
+        valueType = 'null';
+      } else {
+        valueType = typeof value;
+      }
+      if (valueType === 'array') {
+        let entity_items = [];
+        
+        for (let item of lineItems) {
+          // log('Single Item: ', item);
+          let entity_item = {};
+          for (let item_properties of item) {
+            entity_item[item_properties.name] = item_properties.rawValue
+          }
+          entity_items.push(entity_item)
+        }
+        log('entity_items:');
+        log(entity_items);
+        entity[key] = entity_items;
+        log(entity);
+      }
+    }
+  }
+  catch (err) {
+    log(err)
+  }
+  // log(entity_items);
+  return entity;
+}
+
+module.exports = {
+  auth_token: async function () {
     return await get_token();
   },
-  schema_id: async function(){
+  schema_id: async function () {
     return await get_schema();
   },
-  post_job: async function(pdf, fileName, auth_token){
+  post_job: async function (pdf, fileName, auth_token) {
     return await post_job(pdf, fileName, auth_token)
   },
-  get_job_status: async function(job_id, auth_token){
+  get_job_status: async function (job_id, auth_token) {
     return await get_job_status(job_id, auth_token);
-  }, 
-  entity_mapping_head: async function(dox_output, entity) {
+  },
+  entity_mapping_head: async function (dox_output, entity) {
     return await entity_mapping_head_def(dox_output, entity)
   },
-  entity_mapping_item: async function(dox_output, entity) {
+  entity_mapping_item: async function (dox_output, entity) {
     return await entity_mapping_item_def(dox_output, entity)
-  }  
+  }
 }
